6f2fd3de635b99807742f1482b6c84a5789f6a41
SimonIgnat <Signat@kth.se>
2017-02-23 14:04:34 +0100

    EulerDone

--- Lab2/Test_1.m
+++ Lab2/Test_1.m
@@ -2,18 +2,20 @@
 alpha = 2;
 u0 = [0.1 0];
 T = 10;
-n = 1000;
+n = 100000;
 [t,y] = feuler(u0,T,n,alpha);
 plot(t,y(:,1),'b',t,y(:,2),'r');
 
 %% 
+
+format long
 alpha = 2;
 u0 = [0.1 0];
 T = 10;
 
-N = [100, 500, 1000, 2000];
+N = [1000, 2000];
 
-kvot = zeros(1, length(N)
+kvot = zeros(1, length(N));
 i = 1;
 for n = N
     factorsOfn = [n, 2*n, 4*n];
@@ -22,13 +24,20 @@ for n = N
     j = 1;
     for k = factorsOfn
         [t,y] = feuler(u0,T,(n*k),alpha);
-        x(i) = y(end,1);
+        x(j) = y(end,1);
         j = j+1;
     end
-    kvot(i) =  (x(3) - x(2))/(x(2) - x(1));    
+    kvot(i) =  (x(3) - x(2))/(x(2) - x(1));   
+    i = i + 1;
 end
 
+%%
 
+
+
+for i = 1:length(N)
+    disp(sprintf('N = %f, kvot = %f', N(i), kvot(i)));
+end
 
 
 
