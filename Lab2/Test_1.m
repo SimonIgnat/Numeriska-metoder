52decf15398f7f8d8cf9a3e7638b3b3af17e7501
SimonIgnat <Signat@kth.se>
2017-02-24 13:25:06 +0100

    Subplots d)

--- Lab2/Test_1.m
+++ Lab2/Test_1.m
@@ -45,3 +45,38 @@ plot(t,y(:,1),'b',t,y(:,2),'r');
 
 
 %%
+alpha = 2;
+alpha_nonlinear = 0.05;
+T = 10;
+n = 1000;
+
+phis = [0.1, 1, 1.5, 3.1];
+
+i = 1;
+figure(1)
+for phi = phis
+    u0 = [phi,0];
+    [t_lin, y_lin] = rk4(u0,T,n,alpha);
+    [t_nlin, y_nlin] = rk4olin(u0,T,n,alpha);
+    
+    subplot(2,2,i)
+    hold on
+    plot(t_lin,y_lin(:,1), 'DisplayName', 'linear');
+    plot(t_nlin, y_nlin(:,1), 'DisplayName', 'non-linear');
+    
+    legend('show')
+    title(sprintf('Phi_0 = %.f',phi));
+    hold off
+    
+    i = i +1;
+end
+
+
+
+
+
+
+
+
+
+
