e05d281ceb11b776b5aa27fb0c9953b968d3464e
mregard <micke.regard@gmail.com>
2017-03-20 19:48:13 +0100

    Rk4

--- Projekt/Projekt_1.m
+++ Projekt/Projekt_1.m
@@ -1,1 +1,10 @@
-x =1;
+% L = L0/(1 + I^2);
+%  U = LdI/dt;
+% I = -CdU/dt
+% Använd RK-4
+% Visa att följande uttryck kan härledas.
+% d^2I/dt = 2I/(1+I^2)(dI/dt)^2 - I(1+I^2)/L0C
+% t=0 ,I = 0 dI/dt = U0/L0.
+% E(t) = CU(t)^2 + L0ln(1+I(t)^2) = konstant
+L0 = 1;
+C = 1*10^-6;
