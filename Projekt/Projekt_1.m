d23f52f5b7458a30195f2a010aeadc89ff41b43c
SimonIgnat <Signat@kth.se>
2017-04-14 16:23:56 +0200

    Add files via upload

--- Projekt/Projekt_1.m
+++ Projekt/Projekt_1.m
@@ -27,7 +27,7 @@ for u0 = uVec
     plot(t,y(:,1),'b');
     title(sprintf('I(t) with U0 = %.f',u0));
     plotCounter = plotCounter + 1;
-    xlim([0 0.01])
+    xlim([0 0.015])
    
     subplot(3,2,plotCounter)
     plot(t,C*(L0./(1+y(:,1).^2).*y(:,2)).^2+L0*log(1+y(:,1).^2),'g')
@@ -37,17 +37,71 @@ end
 
 % ta fram frekvensen p� sinusen --> T f�r att ber�kna fouriertransform.
 
-
+ 
 % a_k = 2/T int I(t) sin(kwt)dt'
 
-%%
-k = 14;
-a = zeros(1,k);
 
-for k = 1:14
-    a(k) = 2*(y(,1)*sin(k*2*pi/T))
+%% Estimating max current and period at transient
+
+T = 0.3*0.05;
+n = 5000*0.05;
+
+uVec = [240 1200 2400];
+plotCounter = 0;
+
+for u0 = uVec
+    fprintf('U_0 = %.0f', u0)
+    I0 = [0 u0/L0];
+    [t,y] = RK4_projekt(I0,T,n, L0,C);
+    
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % Calculate Imax
+    maxIdx = find(y(:,1)==max(y(:,1)));
+    % Fit second degree polynomial around maximum point
+    poly = polyfit(t(maxIdx-1:maxIdx+1)', y(maxIdx-1:maxIdx+1,1),2);
+    Imax1 = max(polyval(poly,t))
+    
+    % Increase step length to evaluate error
+    poly = polyfit(t(maxIdx-2:maxIdx+2)', y(maxIdx-2:maxIdx+2,1),2);
+    Imax2 = max(polyval(poly,t));
+    
+    Etrunk_imax = abs(Imax1-Imax2)
+    
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % Calculate T
+    notFoundT = true
+    
+    
+    i = 2; % To avoid first crossing of zero (sign(0) = 0)
+    while notFoundT
+        if(sign(y(i,1)) ~= sign(y(i+1,1)))
+            period = t(i) + t(i+1);
+            notFoundT = false;
+        end
+        i = i + 1;
+    end
+    period
+    
+    
+    [t,y] = RK4_projekt(I0,T,n/2, L0,C);
+    
+    notFoundT = true
+    
+    i = 2; % To avoid first crossing of zero (sign(0) = 0)
+    while notFoundT
+        if(sign(y(i,1)) ~= sign(y(i+1,1)))
+            period2h = t(i) + t(i+1);
+            notFoundT = false;
+        end
+        i = i + 1;
+    end
+    
+    EtrunkT = abs(period - period2h)
+
 end
 
+
+ 
 
 
 
